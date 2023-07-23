8a334c690016eb1f595bdb9a9bc8c09f46526b65
Fledge Shiu <xzk0701@gmail.com>
2023-07-23 18:18:10 -0400

    Update.

---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,20 @@
+return {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = {
+        "jay-babu/mason-null-ls.nvim",
+    },
+    config = function()
+        local tools = {
+            "black",
+        }
+
+        require("mason-null-ls").setup({
+            ensure_installed = tools,
+            handlers = {},
+        })
+
+        require("null-ls").setup({
+            sources = {},
+        })
+    end
+}
